4cc46c93bf79d0620acfbf8051d694332ca7a2e7
RohanJacob23 <rohankoshyjacob@gmail.com>
2023-08-22 13:45:18 +0530

    First Commit

---
+++ src/components/CartSlider.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import { ScrollArea } from "./ui/scroll-area";
+import { Input } from "./ui/input";
+
+export default function CartSlider() {
+  const tempCart = [
+    {
+      item: "Fruits and Vegetables",
+      itemList: [{ item: "Avacado" }, { item: "Pre-cooked corn 450kg" }],
+    },
+    {
+      item: "Meat and Fish",
+      itemList: [
+        { item: "Chicken 1kg" },
+        { item: "Chicken leg box" },
+        { item: "Salmon 1kg" },
+        { item: "Salmon 1kg" },
+        { item: "Salmon 1kg" },
+        { item: "Salmon 1kg" },
+        { item: "Pork fillet 450g" },
+      ],
+    },
+  ];
+  return (
+    <section className="fixed right-0 top-0 flex flex-col bg-[#FFF0DE] h-full w-64 md:w-96 pt-6 md:pt-11">
+      <div className="flex justify-around bg-[#80485B] rounded-3xl text-white px-3 py-5 w-64 md:w-80 h-32 mx-4 md:m-auto self-center">
+        <Image
+          src="/icons/source.svg"
+          width={100}
+          height={100}
+          alt="vector-icon"
+          className="-translate-y-10 w-32 h-32"
+        />
+        <div className="flex flex-col h-fit space-y-3.5">
+          <p className="w-40 font-bold text-base">
+            Didn&apos;t find what you need?
+          </p>
+          <Button className="bg-white text-black rounded-xl hover:text-white">
+            Add Item
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center text-2xl font-bold mt-8 md:mt-11 mb-10 pl-6 pr-3 md:pl-12 md:pr-8">
+        <h1>Shopping list</h1>
+        <Pencil1Icon className="w-6 h-6" />
+      </div>
+
+      <div className="scrollArea flex flex-col py-4 space-y-14 h-full overflow-y-scroll pl-6 pr-3 md:pl-12 md:pr-8">
+        {tempCart.map(({ item, itemList }, index) => (
+          <div key={index}>
+            <h1 className="text-sm font-medium text-[#828282]">{item}</h1>
+            <div className="space-y-4">
+              {itemList.map(({ item }, index) => (
+                <div key={index} className="flex items-center justify-between">
+                  <h1 className="text-sm md:text-lg font-medium">{item}</h1>
+                  <p className="rounded-3xl border-2 border-[#F9A109] px-5 py-2 text-[#F9A109] font-bold">
+                    3 pcs
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* bottom section */}
+      <div className="flex h-32 w-full bg-white items-center p-5 md:px-10 md:py-9">
+        <Input
+          type="text"
+          placeholder="Enter a name"
+          className="border-[#F9A109] border-r-0 h-16 focus-visible:outline-none focus-visible:ring-0"
+        />
+        <Button className="bg-[#F9A109] text-white h-16 -ml-2 px-6 py-4">
+          Save
+        </Button>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Sidenav.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Badge } from "./ui/badge";
+
+export default function Sidenav() {
+  const pathname = usePathname();
+  return (
+    <section className="relative z-50 flex flex-col justify-between items-center min-w-[3.84rem] md:min-w-[6rem] h-screen bg-white py-11">
+      <Image
+        src="/logo/logo.svg"
+        alt="logo"
+        width={50}
+        height={50}
+        className="w-11 h-11"
+        priority={true}
+      />
+
+      <div className="flex flex-col items-center gap-16">
+        <TooltipProvider delayDuration={300}>
+          <div className="relative group">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" asChild>
+                  <Link href="/">
+                    <Image
+                      src="/icons/list.png"
+                      alt="list"
+                      width={50}
+                      height={50}
+                      className="w-6 h-6"
+                    />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>items</p>
+              </TooltipContent>
+            </Tooltip>
+            <div
+              className={`${
+                pathname === "/" ? "block" : "hidden"
+              } group-hover:block absolute top-1/2 -translate-y-1/2 -left-1/4 -translate-x-full w-1.5 h-11 bg-[#F9A109] rounded-r`}
+            ></div>
+          </div>
+          <div className="relative group">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" asChild>
+                  <Link href="/history">
+                    <Image
+                      src="/icons/reload.png"
+                      alt="list"
+                      width={50}
+                      height={50}
+                      className="w-6 h-6"
+                    />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>history</p>
+              </TooltipContent>
+            </Tooltip>
+            <div
+              className={`${
+                pathname.includes("history") ? "block" : "hidden"
+              } group-hover:block absolute top-1/2 -translate-y-1/2 -left-1/4 -translate-x-full w-1.5 h-11 bg-[#F9A109] rounded-r`}
+            ></div>
+          </div>
+          <div className="relative group">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" asChild>
+                  <Link href="/home">
+                    <Image
+                      src="/icons/insert_chart.png"
+                      alt="list"
+                      width={50}
+                      height={50}
+                      className="w-6 h-6"
+                    />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>statistics</p>
+              </TooltipContent>
+            </Tooltip>
+            <div
+              className={`${
+                pathname.includes("statistics") ? "block" : "hidden"
+              } group-hover:block absolute top-1/2 -translate-y-1/2 -left-1/4 -translate-x-full w-1.5 h-11 bg-[#F9A109] rounded-r`}
+            ></div>
+          </div>
+        </TooltipProvider>
+      </div>
+      <div className="relative flex items-center justify-center rounded-full bg-[#F9A109] w-11 h-11">
+        <Image
+          src="/icons/cart.png"
+          alt="menu"
+          width={50}
+          height={50}
+          className="w-5 h-5"
+        />
+        <Badge className="absolute -top-2 -right-2 bg-[#EB5757]">3</Badge>
+      </div>
+    </section>
+  );
+}
